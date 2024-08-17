3df863ef07e7f980e78dc02119eef9c0e0b21a26
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-08-17 21:55:08 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div className="pt-24 flex flex-col justify-center items-center space-y-8">
+      <h1 className="text-center text-6xl font-extrabold text-gray-900 dark:text-black-100">
+        Milestone 1: Installation & HelloWorld
+      </h1>
+ <div className="bg-gradient-to-r from-[#0033cc] via-[#cc0077] to-[#4b0082] mt-8 h-72 w-80 rounded-3xl shadow-2xl flex justify-center items-center">
+  <h2 className="text-center font-semibold text-black text-4xl drop-shadow-lg">
+    Hello World
+  </h2>
+</div>
+</div> 
+  )
+}
